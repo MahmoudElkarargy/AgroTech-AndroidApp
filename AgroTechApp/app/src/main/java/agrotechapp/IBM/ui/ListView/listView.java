adafa81ec3326ad48de8820213494ed1cb6b0772
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-09-21 12:48:55 +0200

    modified list view activity

--- AgroTechApp/app/src/main/java/agrotechapp/IBM/ui/ListView/listView.java
+++ AgroTechApp/app/src/main/java/agrotechapp/IBM/ui/ListView/listView.java
@@ -2,17 +2,25 @@ package agrotechapp.IBM.ui.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import agrotechapp.IBM.Dashboard;
 import agrotechapp.IBM.R;
 
 public class listView extends AppCompatActivity {
 
     ListView myListView;
     String[] IDs;
+    ImageView backImageView;
+    TextView backTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +31,23 @@ public class listView extends AppCompatActivity {
         IDs = res.getStringArray(R.array.IDs);
         myListView.setAdapter(new ArrayAdapter<String>(this,R.layout.my_list_detail,IDs));
 
+        backImageView = findViewById(R.id.backImageView);
+        backTextView = findViewById(R.id.backTextView);
+
+        backImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBackIntent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(goBackIntent);
+            }
+        });
+
+        backTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBackIntent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(goBackIntent);
+            }
+        });
     }
 }
