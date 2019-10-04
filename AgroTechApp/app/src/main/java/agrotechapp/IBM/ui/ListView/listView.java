a799aa56000c4f9ea9a176461776990d241da3f6
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-10-04 03:02:27 +0200

    added graph

--- AgroTechApp/app/src/main/java/agrotechapp/IBM/ui/ListView/listView.java
+++ AgroTechApp/app/src/main/java/agrotechapp/IBM/ui/ListView/listView.java
@@ -15,7 +15,30 @@ import android.widget.TextView;
 import android.view.Window;
 
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import agrotechapp.IBM.Dashboard;
 import agrotechapp.IBM.Logic.SensorData;
@@ -33,6 +56,10 @@ public class listView extends AppCompatActivity {
     String[] pHs;
     String[] soil;
     String[] dates;
+
+    private Date dateTimes;
+    private String[] time;
+
 
 
     @Override
@@ -80,6 +107,19 @@ public class listView extends AppCompatActivity {
             }
         });
 
+        drawGraph();
+
+    }
+
+    private class CustomDataEntry extends ValueDataEntry {
+
+        CustomDataEntry(String x, Number value, Number value2, Number value3) {
+            super(x, value);
+            setValue("value2", value2);
+            setValue("value3", value3);
+        }
+
+
     }
 
     private int getNumOfSensors(ArrayList< ArrayList<SensorData>> sensorsData){
@@ -114,6 +154,106 @@ public class listView extends AppCompatActivity {
             }
         }
 
+    }
+
+    private void drawGraph(){
+
+
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        Cartesian cartesian = AnyChart.line();
+
+        cartesian.animation(true);
+
+        cartesian.padding(5d, 10d, 5d, 10d);
+
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                // TODO ystroke
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("");
+
+        cartesian.yAxis(0).title("");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+        List<DataEntry> seriesData = new ArrayList<>();
+
+        try{
+            time = getTimes(dates);
+        }
+        catch (ParseException e){
+            System.out.println("Error");
+        }
+
+        for(int i = 0; i<pHs.length;i++){
+//            System.out.println(time[i]);
+            seriesData.add(new CustomDataEntry(time[i], Float.parseFloat(temps[i]), Float.parseFloat(pHs[i]), Float.parseFloat(soil[i])));
+        }
+
+        Set set = Set.instantiate();
+        set.data(seriesData);
+        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+        Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
+
+        Line series1 = cartesian.line(series1Mapping);
+        series1.name("Temperature");
+        series1.hovered().markers().enabled(true);
+        series1.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series1.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        Line series2 = cartesian.line(series2Mapping);
+        series2.name("pH");
+        series2.hovered().markers().enabled(true);
+        series2.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series2.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        Line series3 = cartesian.line(series3Mapping);
+        series3.name("Soil Moisture");
+        series3.hovered().markers().enabled(true);
+        series3.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series3.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        cartesian.legend().enabled(true);
+        cartesian.legend().fontSize(13d);
+        cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+        anyChartView.setChart(cartesian);
+    }
+
+    private String[] getTimes(String[] dates) throws ParseException {
+        //return times from dates and sort them
+        String[] time = new String[dates.length];
+        double[] timeDouble = new double[dates.length];
+        for(int i =0; i<dates.length; i++){
+            dateTimes = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dates[i]);
+            time[i] = new SimpleDateFormat("H.mm").format(dateTimes);
+            timeDouble[i] = Double.parseDouble(time[i]);
+        }
+        Arrays.sort(timeDouble);
+        time = Arrays.stream(timeDouble).mapToObj(String::valueOf).toArray(String[]::new);
+        return time;
     }
 }
 
